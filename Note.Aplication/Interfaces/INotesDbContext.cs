51e49d5035f59bb674472e99f3c30165f3b39472
6N2P <umnik1985@mail.ru>
2024-04-02 11:33:02 +0500

    Добавил схему бд

---
+++ Note.Aplication/Interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Notes.Domain;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Note.Aplication.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Notes.Domain.Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
